3ac4a1f412d1e363e3a6ba5eb0d9ee8ec6223b27
DanielCamaja <dcamajaig@gmail.com>
2018-11-30 16:59:06 -0600

    cambios

--- app/src/main/java/becalm/com/becalm/fragment/Stress.java
+++ app/src/main/java/becalm/com/becalm/fragment/Stress.java
@@ -1,5 +1,6 @@
 package becalm.com.becalm.fragment;
 
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,10 +8,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import becalm.com.becalm.R;
 
 public class Stress extends Fragment {
+
+    private Button stressboton;
+    MediaPlayer stresssong;
 
     View v;
 
@@ -18,6 +23,20 @@ public class Stress extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.stress, container, false);
+
+        stressboton = (Button) v.findViewById(R.id.stressbutton);
+        stressboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stresssong = MediaPlayer.create(getActivity(), R.raw.meditation2);
+                if (stresssong.isPlaying()){
+                    stresssong.pause();
+                }else{
+                    stresssong.start();
+                }
+            }
+
+        });
 
 
         return v;
--- app/src/main/java/becalm/com/becalm/fragment/UnderStress.java
+++ app/src/main/java/becalm/com/becalm/fragment/UnderStress.java
@@ -1,5 +1,6 @@
 package becalm.com.becalm.fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,20 +8,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import becalm.com.becalm.R;
 
 public class UnderStress extends Fragment {
 
-
+    private Button comenzar;
+    MediaPlayer understress;
     View v;
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        understress.stop();
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.stress_under, container, false);
 
+        comenzar = (Button) v.findViewById(R.id.understressbutton);
+        comenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                understress = MediaPlayer.create(getActivity(), R.raw.meditacion);
+                if (understress.isPlaying()){
+                    understress.pause();
+                }else{
+                    understress.start();
+                }
+            }
+
+        });
+
+
 
         return v;
     }
+
+
 }
