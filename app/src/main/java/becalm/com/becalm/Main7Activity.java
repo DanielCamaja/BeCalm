a3b7e8ee3beb5a7ff4dcf1a333015cf0f3a66f46
DanielCamaja <dcamajaig@gmail.com>
2019-02-11 15:22:24 -0600

    se puede ver como de desplaza el boton solo

---
+++ app/src/main/java/becalm/com/becalm/Main7Activity.java
@@ -0,0 +1,40 @@
+package becalm.com.becalm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main7Activity extends AppCompatActivity {
+
+    TextView resultado;
+
+    int lastScore;
+    int best1, best2, best3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+
+        resultado  = findViewById(R.id.resultado);
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        lastScore = preferences.getInt("lastScore", 0);
+
+
+        resultado.setText("SCORE: " + lastScore);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(Main7Activity.this, Main4Activity.class);
+        startActivity(intent);
+        finish();
+    }
+}
